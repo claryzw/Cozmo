187327b660d8defacd2b0fd24aa88a393918b60b
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-03-28 09:18:28 +0200

    Add files via upload

--- Chatbot With Go/util/util.go
+++ Chatbot With Go/util/util.go
@@ -1,1 +1,18 @@
+//Chatbot with Go by Clarence Itai Msindo
+package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+// Helper function to generate a random integer between min and max (inclusive)
+func randomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
+
+// Helper function to return a random element from a slice of strings
+func RandomString(s []string) string {
+	return s[randomInt(0, len(s)-1)]
+}
