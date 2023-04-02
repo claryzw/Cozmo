3cde4dbb645f40fa7ec0650f89968790e9f28031
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-04-02 05:56:20 +0200

    Add files via upload

---
+++ Chatbot with Python/bot.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm Cozmo, your personal chatbot. How can I help you today?")
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+    response = "Sorry, I didn't understand what you said. Can you please try again?"
+
+    if "hello" in text:
+        response = "Hello there! How can I assist you today?"
+    elif "help" in text:
+        response = "Sure, how can I help you today?"
+    elif "bye" in text:
+        response = "Goodbye! Have a nice day."
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+def main():
+    # create an instance of the Updater class and pass in your bot token
+    updater = Updater('5940769441:AAEgwz-cZ48FNq_LXU8O0kTXx7Sp5vyS0p8', use_context=True)
+
+    # get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # register handlers for commands and messages
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # start polling for updates from Telegram
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
--- Chatbot with Python/main.py
+++ Chatbot with Python/main.py
@@ -1,1 +1,22 @@
+# Chatbot with Python by Clarence Itai Msindo
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 
+# Define a command handler
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hey Whats Up?, I'm Cozmo! How Can I Help?")
+
+# Define a message handler
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+# Set up the updater and dispatcher
+updater = Updater('5940769441:AAEgwz-cZ48FNq_LXU8O0kTXx7Sp5vyS0p8', use_context=True)
+dispatcher = updater.dispatcher
+
+# Add handlers
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(filters.text & ~filters.command, echo))
+
+# Start the bot
+updater.start_polling()
+updater.idle()
