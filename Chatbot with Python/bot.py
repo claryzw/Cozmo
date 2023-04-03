86ac7c140f3e7989648e266c6164049ef7d4ad9a
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-04-03 06:54:38 +0200

    Add files via upload

--- Chatbot with Python/bot.py
+++ Chatbot with Python/bot.py
@@ -1,4 +1,12 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler
+# Chatbot with Python by Clarence Itai Msindo
+import os
+import telebot
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+BOT_TOKEN = os.environ.get('5940769441:AAEgwz-cZ48FNq_LXU8O0kTXx7Sp5vyS0p8')
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm Cozmo, your personal chatbot. How can I help you today?")
@@ -17,6 +25,7 @@ def handle_message(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 def main():
+   def main():
     # create an instance of the Updater class and pass in your bot token
     updater = Updater('5940769441:AAEgwz-cZ48FNq_LXU8O0kTXx7Sp5vyS0p8', use_context=True)
 
@@ -27,8 +36,12 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
+    message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+    dispatcher.add_handler(message_handler)
+
     # start polling for updates from Telegram
     updater.start_polling()
 
 if __name__ == '__main__':
     main()
+
