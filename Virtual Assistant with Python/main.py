b382e8055c05a287da9229c7754ebe1ea57d7a50
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-05-09 21:55:11 +0200

    Update main.py file

--- Virtual Assistant with Python/main.py
+++ Virtual Assistant with Python/main.py
@@ -1,3 +1,4 @@
+# Cozomo Virtual Assistant by Clarence Itai Msindo
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -24,13 +25,17 @@ def takeCommand():
         
         return query
 
+# Function to make Cozmo speak 
 def speak(audio):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
+    # A setter method to determine: [0]=male voice and [1]=female voice.
     engine.setProperty('voice', voices[0].id)
+    # Method for the speaking of the assistant and Blocks while processing all the currently queued commands
     engine.say(audio)
     engine.runAndWait()
 
+# This function is for telling the day of the week
 def tellDay():
     day = datetime.datetime.today().weekday() + 1
     day_dict = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
@@ -40,25 +45,27 @@ def tellDay():
         print(day_of_the_week)
         speak("Today is " + day_of_the_week)
 
+# This method will give the time
 def tellTime():
     time = str(datetime.datetime.now())
     hour = time[11:13]
     minutes = time[14:16]
     speak("The time is " + hour + " hours and " + minutes + " minutes.")
 
+# This function is for when Cozmo is called it will say hello and then take query.
 def hello():
-    speak("Hello! I am your Cozmo Virtual Assistant. How may I assist you?")
+    speak("Hey Whatsup! I am Cozmo, your virtual assistant. What can I help you with?")
 
 def takeQuery():
     hello()
     
+     # Taking the query and making it into lower case so that most of the times query matches and we get the perfect output
     while True:
         query = takeCommand().lower()
         
         if "open google" in query:
             speak("Opening Google...")
-            # Open Google website
-            # (You can modify this as per your requirement)
+            # Open Google website (You can modify this as per your requirement)
             continue
         
         elif "which day is it" in query:
@@ -81,7 +88,8 @@ def takeQuery():
             speak(result)
         
         elif "tell me your name" in query:
-            speak("I am your Cozmo Virtual Assistant.")
+            speak("My name is Cozmo.")
     
 if __name__ == '__main__':
     takeQuery()
+
